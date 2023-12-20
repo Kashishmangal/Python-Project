52b06c8f60b5999cad55d3a80d727bf3fb6359f6
Kashishmangal <147061857+Kashishmangal@users.noreply.github.com>
2023-12-20 14:11:31 +0530

    Add files via upload

--- Library Management/app.py
+++ Library Management/app.py
@@ -1,57 +1,69 @@
-from utils import database
+movies = []
 
-USER_CHOICE = """
-Enter:
-- 'a' to add a new book
-- 'l' to list of all books
-- 'r' to mark a book as read
-- 'd' to delete a book
-- 'q' to quit
-
-Your choice: """
+"""
+movie={
+    'name':... (str),
+    'director':... (str),
+    'year':... (int)
+}
+"""
 
 
 def menu():
-    user_input = input(USER_CHOICE)
+    user_input = input("Enter 'a' to add a movie, 'l' to see your your movies, 'f' to find a movie, 'q' to quit.")
+
     while user_input != 'q':
         if user_input == 'a':
-            prompt_add_book()
+            add_movie()
         elif user_input == 'l':
-            list_books()
-        elif user_input == 'r':
-            prompt_read_book()
-        elif user_input == 'd':
-            prompt_delete_book()
+            show_movies(movies)
+        elif user_input == 'f':
+            find_movie()
         else:
-            print("Unknown command. Please try again.")
-
-        user_input = input(USER_CHOICE)
-
-
-def prompt_add_book():
-    name = input("Enter the name of the book: ")
-    author = input("Enter the new book author: ")
-
-    database.add_book(name, author)
+            print('Unknown command- Please try again.')
+        user_input = input("\nEnter 'a' to add a movie, 'l' to see your your movies, 'f' to find a movie, 'q' to quit.")
 
 
-def list_books():
-    books = database.get_all_books()
-    for book in books:
-        read = 'YES' if book['read'] else'NO'
-        print(f"{book['name']} by {book['author']}, read: {read}")
+def add_movie():
+    name = input('Enter the movie name: ')
+    director = input('Enter the director name: ')
+    year = input('Enter the release year: ')
+
+    movies.append({
+        'name': name,
+        'director': director,
+        'year': year
+    })
 
 
-def prompt_read_book():
-    name = input("Enter the name of the book you just finished reading: ")
+def show_movies(movies_list):
+    for movie in movies_list:
+        show_movie_details(movie)
 
-    database.mark_book_as_read(name)
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
 
 
-def prompt_delete_book():
-    name = input("Enter the name of the book you wish to delete: ")
+def find_movie():
+    find_by = input("What property of the movie are you looking for? ")
+    looking_for = input("What are you searching for? ")
 
-    database.delete_book(name)
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+    found = []
+
+    for i in items:
+        if finder(i) == expected:
+            found.append(i)
+
+    return found
 
 
 menu()
